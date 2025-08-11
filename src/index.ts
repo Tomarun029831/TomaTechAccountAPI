ac7fa1c5e0f853232fcd1bc897c9e8973f2248c8
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-08-11 14:25:00 +0900

    chore(ALL): yank original code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,57 @@
 const PEPPER = PropertiesService.getScriptProperties().getProperty("PEPPER");
-const MAX_COLUMN = 4;
+const ACCOUNT_INFO_LEN = 4;
 
-function logAccess(mode: string, username: string, result: boolean) {
+/**
+1. 認証トークン（JWTなど）
+クライアントはログイン情報を送信し、GASがJWTを発行
+
+クライアントはAPI呼び出しにJWTを必ずヘッダーに付ける
+
+GASはJWTを検証し、期限切れ・不正なトークンなら拒否
+
+2. API署名検証（HMAC）
+APIリクエストにはパラメータをHMAC署名付きで送信
+
+GASは受け取り次第、署名の正当性を検証し改ざんを防止
+
+4. レートリミット
+GASのCacheServiceを使い、ユーザーごと・IPごとにAPIコール回数を管理
+
+一定期間にアクセス回数を超えたら拒否する制御を入れる
+*/
+
+function doPost(e: { parameter: { mode: any; accountData: { username: any; password: any; }; }; headers: { [x: string]: any; }; body: any; }): GoogleAppsScript.Content.TextOutput {
+    const mode = e.parameter.mode;
+    const plainUsername = e.parameter.accountData.username;
+    const plainPassword = e.parameter.accountData.password;
+    const plainToken = e.headers["authorization"];
+    const trackedData = e.body;
+    let result: boolean = false;
+
+    switch (mode) {
+        case "CREATE":
+            result = createNewAccount(plainUsername, plainPassword);
+            break;
+        case "AUTHENTICATE":
+            result = authenticateAccount(plainUsername, plainPassword);
+            break;
+        case "PUSH":
+            result = pushTrackedData(plainToken, trackedData);
+            break;
+        case "PULL":
+            result = pullTrackedData(plainToken);
+            break;
+        default:
+            break;
+    }
+
+    logAccess(mode, plainUsername, result);
+
+    const responseJSON = JSON.stringify({ message: "post API", result: result });
+    return ContentService.createTextOutput(responseJSON).setMimeType(ContentService.MimeType.JSON);
+}
+
+function logAccess /**: void*/(mode: string, username: string, result: boolean): void {
     const ss = SpreadsheetApp.getActiveSpreadsheet();
     let sheet = ss.getSheetByName("Log");
 
@@ -19,45 +69,23 @@ function logAccess(mode: string, username: string, result: boolean) {
     ]);
 }
 
-
-function doPost(e: { parameter: { mode: any; username: any; password: any; }; }) {
-    const mode = e.parameter.mode;
-    const plainUsername = e.parameter.username;
-    const plainPassword = e.parameter.password;
-    let result: boolean = false;
-
-    switch (mode) {
-        case "CREATE":
-            result = createNewAccount(plainUsername, plainPassword);
-            break;
-        case "AUTHENTICATE":
-            result = authenticateAccount(plainUsername, plainPassword);
-            break;
-        default:
-            break;
-    }
-
-    logAccess(mode, plainUsername, result);
-
-    const responseJSON = JSON.stringify({ message: "post API", result: result });
-
-    return ContentService.createTextOutput(responseJSON).setMimeType(ContentService.MimeType.JSON);
-}
-
-function createNewAccount /*: boolean*/(plainUsername: string, plainPassword: string): boolean {
+function createNewAccount(plainUsername: string, plainPassword: string): boolean {
     if (plainUsername == null || plainPassword == null) { throw new ReferenceError("user_name or password is null or undefined"); }
 
     const foundRow = searchRowIndexOfMatchedAccount(plainUsername);
     if (foundRow !== -1) { return false; }
 
-    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Account");
-    if (sheet === null) return false;
-    const lastRow = sheet.getLastRow() + 1;
-    const range = sheet.getRange(lastRow, 1, 1, MAX_COLUMN);
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    let sheet = ss.getSheetByName("Account");
+    if (!sheet) {
+        sheet = ss.insertSheet("Account");
+        sheet.appendRow(["Uuid", "Username", "Password", "Salt"]);
+    }
 
-    // Uuid
+    const lastRow = sheet.getLastRow() + 1;
+    const range = sheet.getRange(lastRow, 1, 1, ACCOUNT_INFO_LEN);
+
     const Uuid = Utilities.getUuid();
-    // password
     const salt = Utilities.getUuid();
     const authenticPassword = hashPassword(plainPassword, salt);
 
@@ -67,83 +95,67 @@ function createNewAccount /*: boolean*/(plainUsername: string, plainPassword: st
     return true;
 }
 
-function authenticateAccount /*: boolean*/(plainUsername: string, plainPassword: string): boolean {
+function authenticateAccount(plainUsername: string, plainPassword: string): boolean {
     const fonudRow = searchRowIndexOfMatchedAccount(plainUsername);
     if (fonudRow === -1) { return false; }
 
-    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Account");
-    if (sheet === null) return false;
-    const range = sheet.getRange(fonudRow, 1, 1, MAX_COLUMN).getValues().flat();
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    let sheet = ss.getSheetByName("Account");
+    if (!sheet) {
+        sheet = ss.insertSheet("Account");
+        sheet.appendRow(["Uuid", "Username", "Password", "Salt"]);
+    }
+    const range = sheet.getRange(fonudRow, 1, 1, ACCOUNT_INFO_LEN).getValues().flat();
 
-    const storedUuid = range[0];
-    const storedUsername = range[1];
+    // const storedUuid = range[0];
+    // const storedUsername = range[1];
     const storedPassword = range[2];
     const storedSalt = range[3];
 
     return hashPassword(plainPassword, storedSalt) === storedPassword;
 }
 
-function hashPassword /*: string*/(plainPassword: string, plain_salt: string): string {
+function pullTrackedData(token: string): boolean {
+    if (!verifyToken(token)) {
+        throw new Error("Invalid or expired token");
+    }
+    // TODO: implement
+
+    return false;
+}
+
+
+function pushTrackedData(token: string, trackedData: string): boolean {
+    console.log("pushTrackedData called with token: " + token + " trackedData " + trackedData);
+    if (!verifyToken(token)) {
+        throw new Error("Invalid or expired token");
+    }
+    // TODO: implement
+
+    return false;
+}
+
+function hashPassword(plainPassword: string, plain_salt: string): string {
     const concatnated = plainPassword + plain_salt + PEPPER;
     const rawHash = Utilities.computeDigest(Utilities.DigestAlgorithm.SHA_256, concatnated);
     return rawHash.map(b => ('0' + (b & 0xFF).toString(16)).slice(-2)).join('');
 }
 
-function searchRowIndexOfMatchedAccount /*: int*/(user_name: string): number {
+function verifyToken(token: string): boolean {
+    console.log("verifyToken called with " + token);
+    return false;
+}
+
+function searchRowIndexOfMatchedAccount(username: string): number {
     const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
     const range = spreadsheet.getRangeByName("Username");
     if (range === null) return -1;
     const usernames = range.getValues().flat();
-    const index = usernames.findIndex(name => name === user_name);
+    const index = usernames.findIndex(name => name === username);
     return (index === -1 && index <= 1) ? -1 : index + 1;
 }
 
-
-/*
-{
-    "message": "get API",
-    "para_e": {
-        "queryString": "para1=1",
-        "contextPath": "",
-        "contentLength": -1,
-        "parameters": {
-            "para1": [
-                "1"
-            ]
-        },
-        "parameter": {
-            "para1": "1"
-        }
-    }
-}
-*/
-
-/*
-{
-    "message": "post API",
-    "para_e": {
-        "contentLength": 7,
-        "parameter": {
-            "para1": "1"
-        },
-        "contextPath": "",
-        "queryString": "",
-        "parameters": {
-            "para1": [
-                "1"
-            ]
-        },
-        "postData": {
-            "contents": "para1=1",
-            "length": 7,
-            "name": "postData",
-            "type": "application/x-www-form-urlencoded"
-        }
-    }
-}
-*/
-
-/*
+/**
 function initAllProperties(){
   PropertiesService.getScriptProperties().deleteAllProperties();
   initPepper();
